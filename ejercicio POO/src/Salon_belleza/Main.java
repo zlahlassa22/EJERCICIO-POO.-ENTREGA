afe06b156317c1908e9348b52d818a9fcab71cb7
zakaria <zlahlassa22dw@ikzubirimanteo.com>
2023-01-10 18:14:45 +0100

    clase Main terminada

--- ejercicio POO/src/Salon_belleza/Main.java
+++ ejercicio POO/src/Salon_belleza/Main.java
@@ -4,19 +4,29 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-        Salon salon = new Salon(10, 10);
-        Cliente cli1= new Cliente("Maria","12345A");
-        Cliente cli2= new Cliente("Fer","12345B");
-        Cliente cli3= new Cliente("Wanda","12345C");
-        salon.añadircliente(cli1);
-        salon.añadircliente(cli2);
-        salon.añadircliente(cli3);
-        salon.setmembresia("12345A","premium");
-        salon.setmembresia("12345B","oro");
+        Cliente cli1=new Cliente("zaka","12345x",true,"oro");
+        Cliente cli2=new Cliente("zakaria2","12345Y",true,"premium");
+        Cliente cli3 = new Cliente("Zaka3","12345n");
         Date date = new Date(2021, 6, 21);
-        salon.anadirVisita("12345A",date,96);
-        salon.mostrarClientes();
-        salon.mostrarVisitas();
+        Visita vis1 = new Visita(cli1 , date);
+        Visita vis2 = new Visita(cli2 , date);
+        Visita vis3 = new Visita(cli3 , date);
+        vis1.setImportetotal(1000);
+        vis2.setImportetotal(100);
+        vis3.setImportetotal(50);
+       Salon sal = new Salon(3 , 3);
+        sal.añadircliente(cli3);
+        sal.añadircliente(cli2);
+        sal.añadircliente(cli1);
+        sal.anadirVisita(vis2);
+        sal.anadirVisita(vis3);
+        sal.anadirVisita(vis1);
+        System.out.println(sal.getCliente("12345x"));
+        sal.mostrarClientes();
+        sal.bajacliente(cli2);
+        System.out.println(cli2.getSocio());
+        sal.mostrarVisitas();
+
     }
 }
 
